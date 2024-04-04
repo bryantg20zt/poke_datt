0a114cfa61347965b04edda5b0a0b28f0eff0392
bryantg20zt <bryanturrubiates@outlook.com>
2024-04-04 18:16:52 -0500

    feat: (PokeRandom) Addded info

--- src/views/PokeRandom/PokeRandom.jsx
+++ src/views/PokeRandom/PokeRandom.jsx
@@ -6,7 +6,16 @@ export function PokeRandom () {
   return (
     <main className={Styles.mainContainer}>
       <PokeHeader />
-      <PokeInfoSection title='SQUIRTLE' />
+      <div className={Styles.sectionInformation}>
+        <section>
+          <PokeInfoSection title='Squirtle' type='pokemon' />
+          <PokeInfoSection title='Stats Base' type='stats' />
+        </section>
+        <section>
+          <PokeInfoSection title='Habilidades' type='abilities' />
+          <PokeInfoSection title='Fisionomia' type='fisio' />
+        </section>
+      </div>
     </main>
   )
 }
--- src/views/PokeRandom/components/PokeInfoSection.jsx
+++ src/views/PokeRandom/components/PokeInfoSection.jsx
@@ -1,11 +1,12 @@
 import { PokeStat } from '@components/PokeInfo/Stats.jsx'
 import { IoIosWater } from 'react-icons/io'
+import Styles from './PokeInfoSection.module.css'
 
-export function PokeInfoSection ({ title }) {
+function PokemonSection ({ title, pokemon }) {
   return (
     <section>
-      <h2>{title}</h2>
-      <div>
+      <h2 className={Styles.title}>{title}</h2>
+      <div className={Styles.containerItems}>
         <PokeStat>
           <IoIosWater />
           <p>Agua</p>
@@ -13,4 +14,81 @@ export function PokeInfoSection ({ title }) {
       </div>
     </section>
   )
+}
+
+function Stats ({ title, stats }) {
+  return (
+    <section>
+      <h2 className={Styles.title}>{title}</h2>
+      <div className={Styles.containerItems}>
+        <PokeStat>
+          <IoIosWater />
+          <p>39</p>
+        </PokeStat>
+        <PokeStat>
+          <IoIosWater />
+          <p>39</p>
+        </PokeStat>
+        <PokeStat>
+          <IoIosWater />
+          <p>39</p>
+        </PokeStat>
+        <PokeStat>
+          <IoIosWater />
+          <p>39</p>
+        </PokeStat>
+        <PokeStat>
+          <IoIosWater />
+          <p>39</p>
+        </PokeStat>
+        <PokeStat>
+          <IoIosWater />
+          <p>39</p>
+        </PokeStat>
+      </div>
+    </section>
+  )
+}
+
+function Abilities ({ title }) {
+  return (
+    <section>
+      <h2 className={Styles.title}>{title}</h2>
+      <div className={Styles.containerItems}>
+        <PokeStat>
+          <IoIosWater />
+          <p>39</p>
+        </PokeStat>
+        <PokeStat>
+          <IoIosWater />
+          <p>39</p>
+        </PokeStat>
+      </div>
+    </section>
+  )
+}
+
+function PokeFisio ({ title }) {
+  return (
+    <section>
+      <h2 className={Styles.title}>{title}</h2>
+      <div className={Styles.containerItems}>
+        <PokeStat>
+          <IoIosWater />
+          <p>6 inch</p>
+        </PokeStat>
+        <PokeStat>
+          <IoIosWater />
+          <p>80 kg</p>
+        </PokeStat>
+      </div>
+    </section>
+  )
+}
+
+export function PokeInfoSection ({ title, type }) {
+  if (type === 'pokemon') return <PokemonSection title={title} />
+  if (type === 'stats') return <Stats title={title} />
+  if (type === 'abilities') return <Abilities title={title} />
+  if (type === 'fisio') return <PokeFisio title={title} />
 }
